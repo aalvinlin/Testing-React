513d849981485dcbca628a164f65f4b9cb1819b8
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-01-27 13:47:22 -0800

    component mounting and button click tests working

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,12 +1,35 @@
 import React from "react";
 import {render, fireEvent, wait } from "@testing-library/react";
-import {StarWarsCharacters} from "./StarWarsCharacters";
+import StarWarsCharacters from "./StarWarsCharacters";
 import {getData as mockGetData} from "../api/getData";
 
-TextDecoderStream("renders the StarWarsCharacters component", () => {
-
-    const { test } = render(<StarWarsCharacters />);
-
-
-
+// component mounting tests
+test("renders the StarWarsCharacters component", () => {
+    render(<StarWarsCharacters />);
 })
+
+test("renders the Previous button", () => {
+    const {queryByText} = render(<StarWarsCharacters />);
+    const previousButton = queryByText(/previous/i);
+})
+
+test("renders the Next button", () => {
+    const {queryByText} = render(<StarWarsCharacters />);
+    const nextButton = queryByText(/next/i);
+})
+
+// button click tests
+test("Previous button can be clicked", () => {
+    const {queryByText} = render(<StarWarsCharacters />);
+    const previousButton = queryByText(/previous/i);
+    fireEvent.click(previousButton);
+})
+
+test("Next button can be clicked", () => {
+    const {queryByText} = render(<StarWarsCharacters />);
+    const nextButton = queryByText(/next/i);
+    fireEvent.click(nextButton);
+})
+
+// API call tests with mockGetData
+
